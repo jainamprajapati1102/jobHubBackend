de99a06d9edd08c49b6d59cb9dfcf36ebaa83633
unknown <jainamprajapati1930@gmail.com>
2023-04-17 00:58:08 +0530

    8th commit

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true)
-const DB = mongoose.connect("mongodb://0.0.0.1:27017/jobshub");
+const DB = mongoose.connect("mongodb://127.0.0.1:27017/jobshub");
 if (DB) {
     console.log("connection Successful!!");
 } else {
